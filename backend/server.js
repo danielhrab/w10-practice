10a5d6c0ef40239cf267d1e10e06f297cf318d15
danielhrab <65764799+danielhrab@users.noreply.github.com>
2023-04-05 13:21:06 +0200

    backend2

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
+const { response } = require('express')
 const express = require('express')
 const path = require('path')
+const fs = require('fs')
 const app = express()
 const port = 9000
 
@@ -8,6 +10,50 @@ app.get('/', (req, res) => {
 })
 
 app.use('/public', express.static(`${__dirname}/../frontend/public`))
+
+app.get('/data', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/data/data.json`))
+})
+
+app.get('/data/:id', (req, res) => {
+  console.log(req.params)
+	try {
+		const searchId = parseInt(req.params.id)
+		/* console.log(searchId) */
+
+		if(isNaN(searchId)){
+			res.status(418).send('Not a number')
+		} else {
+
+			fs.readFile(`data/data.json`, (err, data) => {
+				let result = null
+				const fileData = JSON.parse(data)
+				/* console.log(fileData) */
+				for (let i = 0; i < fileData.length; i++) {
+				const element = fileData[i];
+					if(fileData[i].id === searchId) {
+						console.log(element)
+						result = element
+					}
+				}
+				if(result === null){
+					res.status(404).send("Nincs ilyen user WAZZE!")
+				}	else {
+					res.send(result)
+				}	
+			}) 
+		}
+
+	} catch(error){
+		console.log(error)
+		res.send("Elbénáztuk!")
+	}
+})
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`http://127.0.0.1:${port}`)
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,1 +1,15 @@
-console.log('Hello World!')
+fetch('http://127.0.0.1:9000/data')
+.then(response => {
+  console.log(response)
+  if (response.status === 201) {
+    console.log('ok')
+  }
+  return response.json()
+})
+.then(responseJson => {
+  const data= responseJson
+  data.forEach(element => {
+    console.log(element.name)
+    document.querySelector('#root').insertAdjacentHTML("beforeend",`<h2>${element.name}</h2>`)
+  });
+})
